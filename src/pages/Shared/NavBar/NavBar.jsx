ab3d5aaa486273e907ded01c4dc7c49ac1783eaa
Ariful Islam Razu <learnicmind@gmail.com>
2023-06-10 03:18:15 +0600

    footer added

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -22,25 +22,25 @@ const NavBar = () => {
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
                                 </svg>
                             </label>
-                            <ul tabIndex={0} className="menu text-black menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <ul tabIndex={0} className="menu text-[#59dae9] text-xl menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                                 {navLinks}
                             </ul>
                         </div>
 
                         <Link to='/'>
                             <button className="btn btn-ghost normal-case ">
-                                <span className='md:text-3xl text-gray-200 tracking-wide '>Rythmic</span>
+                                <span className='md:text-3xl text-2xl text-[#48a5af] tracking-wide font-bold'>RyThmic</span>
                             </button>
                         </Link>
 
                     </div>
-                    <div className="navbar-center text-white  hidden lg:flex">
+                    <div className="navbar-center text-[#48a5af] hidden lg:flex">
                         <ul className="menu menu-horizontal flex gap-2">
                             {navLinks}
                         </ul>
                     </div>
                     <div className="navbar-end flex items-center gap-2"> <img  className="w-10 rounded-full" src="https://as1.ftcdn.net/v2/jpg/01/49/81/52/1000_F_149815203_Og27WLpK7bCVTz9uDRic1bSxtPnFGguV.jpg" />
-                    <button className="btn bg-[#E8E8E8] border-0 border-[#59dae9] border-b-4 hover:bg-[#111827] hover:border-[#59dae9] text-black hover:text-[#59dae9]">Login</button>
+                    <button className="btn bg-[#E8E8E8] border-0 border-[#48a5af] border-t-4 hover:bg-[#111827] hover:border-[#59dae9] text-black hover:text-[#59dae9]">Login</button>
                     </div>
                 </div>
             </div>
