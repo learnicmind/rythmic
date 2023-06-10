c263c792842b3a992c5da864efac4d40ac42049b
Ariful Islam Razu <learnicmind@gmail.com>
2023-06-10 11:50:05 +0600

    banner added

---
+++ src/pages/Home/Home/Banner/Banner.jsx
@@ -0,0 +1,38 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import img1 from '../../../../assets/banner/img1.png'
+import img2 from '../../../../assets/banner/img2.png'
+import img3 from '../../../../assets/banner/img3.png'
+import img4 from '../../../../assets/banner/img4.png'
+import img5 from '../../../../assets/banner/img5.png'
+import img6 from '../../../../assets/banner/img6.png'
+
+
+const Banner = () => {
+    return (
+        <div>
+            <Carousel>
+            <div>
+                    <img src={img5} />
+                </div>
+                <div>
+                    <img src={img1} />
+                </div>
+                <div>
+                    <img src={img3} />
+                </div>
+                <div>
+                    <img src={img4} />
+                </div>
+                <div>
+                    <img src={img2} />
+                </div>
+                <div>
+                    <img src={img6} />
+                </div>
+            </Carousel>
+        </div>
+    );
+};
+
+export default Banner;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Home from "../pages/Home/Home/Home/Home";
 
 
 
@@ -9,7 +10,8 @@ export const router = createBrowserRouter([
         element: <Main></Main>,
         children: [
             {
-
+                path: '/',
+                element: <Home></Home>
             }
         ]
     }
