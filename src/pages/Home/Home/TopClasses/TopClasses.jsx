626b61248f6a3f17d6c106e582893e648cf9dfaa
Ariful Islam Razu <learnicmind@gmail.com>
2023-06-12 02:11:51 +0600

    instructors details added in instructor detail page

--- src/pages/Home/Home/TopClasses/TopClasses.jsx
+++ src/pages/Home/Home/TopClasses/TopClasses.jsx
@@ -11,8 +11,8 @@ const TopClasses = () => {
             <p className="text-center px-20 py-4 font-semibold text-emerald-50">Welcome to RyThmic, where rhythm meets movement! Our top classes are carefully curated to bring out the best in you, combining the power of music and dance to create an extraordinary experience. Whether you are a seasoned performer or just starting your rhythmic journey, we have the perfect class to help you unlock your full potential. Join us and let the music guide your body as you discover the joy and artistry of dance.</p>
             <div className="grid md:grid-cols-3 gap-6 my-10">
                 {
-                    classes.slice(0, 6).map(item => <div key={item._id} className={item.seats === 0 ? 'bg-red-600 h-full rounded-lg text-white  flex flex-col items-start  hover:scale-105' : 'bg-bage-100 flex flex-col items-start rounded-lg  pb-5 hover:scale-105'} style={{ transition: '0.5s' }}>
-                            <img className="h-48 w-full" src={item.banner} alt="" />
+                    classes.slice(0, 6).map(item => <div key={item._id} className={item.seats === 0 ? 'bg-red-600 rounded-lg text-white  flex flex-col items-start  hover:scale-105' : ' flex flex-col items-start rounded-lg  pb-5 hover:scale-105'} style={{ transition: '0.5s' }}>
+                            <img className="h-48 w-full rounded-sm" src={item.banner} alt="" />
                             <div className="p-3 bg-emerald-50 w-full">
                                 <h1 className="text-xl font-bold">{item.class}</h1>
                                 <h3 className="text-lg font-bold py-1">Instructor: {item.name}</h3>
--- src/pages/Instructors/Instructors.jsx
+++ src/pages/Instructors/Instructors.jsx
@@ -7,14 +7,22 @@ const Instructors = () => {
 
 
     return (
-        <div className="my-20">
-            <h2>this is instructors page</h2>
-            <div>
-                {
-                    instructors.map(ins => <div key={ins._id}>
-                        <img src={ins.image} alt="" />
-                    </div>)
-                }
+        <div >
+
+            <div className="py-28 w-11/12 mx-auto">
+                <h2 className="text-4xl text-center font-bold text-[#59dae9]">Top Instructors</h2>
+                <p className="text-center px-20 py-4 font-semibold text-emerald-50">Meet our exceptional team of instructors at RyThmic who are dedicated to helping you unleash your full potential and guiding you on your rhythmic journey. Each instructor brings a unique blend of talent, expertise, and passion for their craft, creating a supportive and inspiring environment for all our students. Get to know some of our top instructors who will lead you through exhilarating classes and empower you to reach new heights in your dance and movement practice.</p>
+                <div className="grid md:grid-cols-3 gap-8">
+                    {
+                        instructors.map(ins => <div key={ins._id}>
+                            <div className="text-white text-center bg-slate-500 pb-4 rounded-lg">
+                                <img className="w-full h-64 rounded-sm" src={ins.image} alt="" />
+                                <h2 className="text-lg font-bold pt-2">Instructors Name: <span className="text-[#59dae9] text-xl">{ins.name}</span></h2>
+                                <h2 className="text-lg font-semibold">Instructors Email: {ins.email}</h2>
+                            </div>
+                        </div>)
+                    }
+                </div>
             </div>
         </div>
     );
